8754fbed1acb39d10d0932040317dd6aef3e0185
Colin Tracy <colin@aiselng.com>
2019-03-12 01:17:59 -0600

    update playground vars for prod

--- data/schema.js
+++ data/schema.js
@@ -1,18 +1,29 @@
-'use strict';
-const { ApolloServer } = require('apollo-server-express');
-const jwt = require('jsonwebtoken');
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+"use strict";
+const { ApolloServer } = require("apollo-server-express");
+const jwt = require("jsonwebtoken");
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
 
 function authScope(req) {
     const Authorization = req.get("Authorization")
     
     if(Authorization) {
-        const token = Authorization.replace('Bearer ', '')
+        const token = Authorization.replace("Bearer ", "")
         const { userId } = jwt.verify(token, process.env.JWT_SECRET)
         const { idToken } = jwt.decode(token)
         
         return { userId, idToken }
+    }
+}
+
+
+const GRAPHQL_PLAYGROUND_CONFIG = {
+    settings: {
+      "editor.cursorShape": "line",
+      "editor.fontSize": 14,
+      "editor.reuseHeaders": true,
+      "editor.theme": "dark",
+      "editor.endpoint": "/graphql"
     }
 }
 
@@ -22,10 +33,5 @@ module.exports = new ApolloServer({
     context: async ({ req }) => ({
         authScope: await authScope(req)
     }),
-    playground: {
-        endpoint: '/graphql',
-        settings: {
-            'editor.theme' : 'dark'
-        }
-    }
+    playground: process.env.NODE_ENV === "production" ? false : GRAPHQL_PLAYGROUND_CONFIG
 })
